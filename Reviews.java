007970b03c1948b9f6e255e1facfd4c12549db71
derek <dereklee1124@gmail.com>
2023-11-27 09:39:23 -0800

    added user and review changes

--- Reviews.java
+++ Reviews.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Reviews {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Reviews {
+    public static void attachReviewToRating(Connection con,
+                                            int ratingID,
+                                            int restID,
+                                            String review) throws SQLException
+    {
+        Statement statement = con.createStatement();
+        statement.executeUpdate("insert into Reviews(ratingID, restID, review) values("+ratingID+", " + restID + ", '" + review + "');");
+    }
 }
--- Users.java
+++ Users.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Users {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Users
+{
+    public static void addUser(Connection con, String name, String username, String password) throws SQLException
+    {
+        Statement statement = con.createStatement();
+        statement.executeUpdate("insert into Users(name, username, password) values('"+name+"', '" + username + "', '" + password + "');");
+    }
 }
