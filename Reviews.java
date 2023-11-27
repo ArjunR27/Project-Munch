0fa4241b683d35dc47e8338da63a129100c27beb
derek <dereklee1124@gmail.com>
2023-11-27 09:42:30 -0800

    edit

--- Reviews.java
+++ Reviews.java
@@ -13,17 +13,3 @@ public class Reviews {
         statement.executeUpdate("insert into Reviews(ratingID, restID, review) values("+ratingID+", " + restID + ", '" + review + "');");
     }
 }
-
-import java.sql.Connection;
-        import java.sql.ResultSet;
-        import java.sql.SQLException;
-        import java.sql.Statement;
-
-public class Users
-{
-    public static void addUser(Connection con, String name, String username, String password) throws SQLException
-    {
-        Statement statement = con.createStatement();
-        statement.executeUpdate("insert into Users(name, username, password) values('"+name+"', '" + username + "', '" + password + "');");
-    }
-}
